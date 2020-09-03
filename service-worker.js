8a353e750a7c819d29dcfeea580f20f4d877e9b9
Guilherme Fleck Oliveira <guilhermefleckoliveira@gmail.com>
2020-09-03 20:47:52 -0300

    fix github url

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.beb38b5847a562cabb9a60ac89f4236c.js"
+  "/precache-manifest.97019a7a4c5b4a3062201a25fe024fc6.js"
 );
 
 self.addEventListener('message', (event) => {
